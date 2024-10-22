0a6213e2effad689d7a6322c317b6bee88d1a7b2
kithome7 <jsena226@gmail.com>
2024-10-22 17:34:26 +0300

    iOS application folder is added

---
+++ ios-app/po delu/Shared/po_deluApp.swift
@@ -0,0 +1,17 @@
+//
+//  po_deluApp.swift
+//  Shared
+//
+//  Created by qd on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct po_deluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
